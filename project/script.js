bc5d392f5d8f411c5b7b5e0dc29c4f814836d0b0
VAISHNAVI MANDLOI <138960528+vaishnavimandloi08@users.noreply.github.com>
2024-07-20 14:51:17 +0530

    Add files via upload
    Landing Page Animation Use GSAP

---
+++ project/script.js
@@ -0,0 +1,26 @@
+var tl = gsap.timeline({
+    repeat: -1
+});
+
+tl.
+to(".imagecontainer", {
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration: 2,
+    stagger: 2
+},'a')
+.to(".text h1", {
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: 0,
+    color:"grey"
+}, 'a')
+.to(".text h1", {
+    delay: 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%",
+    color: "black"
+}, 'a')
+
+
